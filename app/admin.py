edf2edbecd3994883e14d902585493e81c0de8b7
Marat <lmar@mail.ru>
2019-12-06 12:05:49 +0300

    search and filter added

--- app/admin.py
+++ app/admin.py
@@ -7,9 +7,14 @@ from .forms import ReviewAdminForm
 class CarAdmin(admin.ModelAdmin):
 
     list_display = ('brand', 'model', )
+    list_filter = ('brand', 'model', )
+    search_fields = ('model',)
 
 
 class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('car', 'title',)
+    list_filter = ('title',)
+    search_fields = ('title',)
     form = ReviewAdminForm
 
 
